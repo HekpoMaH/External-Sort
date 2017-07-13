0aa3df20f6813aa46d3fb260a26da7acc552e35d
dobrik <dgg30>
2017-07-13 14:03:59 +0300

    low level optimisations - 20s time

--- ExternalSort.java
+++ ExternalSort.java
@@ -18,21 +18,34 @@ import java.security.NoSuchAlgorithmException;
 import java.util.Map;
 import java.util.HashMap;
 import java.util.TreeMap;
-import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.Collections;
+import java.util.Arrays;
 import java.util.PriorityQueue;
 
 public class ExternalSort {
 
     public static final int RANGE_MIN = (int) -1e5, RANGE_MAX = (int) 1e5;
-    public static final int CNT_MAX = (int) (1e5);
+    public static final int OFFSET = (int) 1e5;
+    public static final int BLOCK_SIZE = (int) (1<<19);
     public static final int INT_BYTES = 4;
 
+    public static void readFile(String fil)throws FileNotFoundException, IOException {
+        RandomAccessFile raf = new RandomAccessFile(fil,"rw");
+        DataInputStream dis = new DataInputStream(
+                new BufferedInputStream(new FileInputStream(raf.getFD())));
+        while(dis.available()>0){
+            System.out.print(dis.readInt());
+        }
+        System.out.println();
+        dis.close();
+        raf.close();
+    }
     public static void countSort(String f1,
             String f2,
             int min,
             int max,
-            HashMap<Integer, Integer> cnt) throws 
+            int[] cnt) throws 
                     FileNotFoundException, IOException {
 
         RandomAccessFile f = new RandomAccessFile(f1,"rw");
@@ -43,16 +56,9 @@ public class ExternalSort {
         DataOutputStream dos = new DataOutputStream(
                 new BufferedOutputStream(new FileOutputStream(f1,false)));
         for(int i = min; i <= max; i++){
-            if (!cnt.containsKey(i)) {
-                continue;
-            }
-            else {
-                int times=cnt.get(i);
-
-                for(int j = 0; j < times; j++){
-                    //System.out.println("Printedddd");
-                    dos.writeInt(i);
-                }
+            for(int j = 0; j < cnt[OFFSET+i]; j++){
+                //System.out.println("Printedddd");
+                dos.writeInt(i);
             }
         }
         dos.flush();
@@ -84,7 +90,7 @@ public class ExternalSort {
             int blockSize) throws FileNotFoundException, IOException {
 
         RandomAccessFile rafFile1 = new RandomAccessFile(file1,"rw");
-        int numOfInts = (int)(rafFile1.length() / (long)INT_BYTES);
+        int numOfInts = (int)(rafFile1.length() >> 2);
         int numOfBlocks = (int)Math.ceil((double)numOfInts / (double)blockSize);
 
         DataOutputStream dos = new DataOutputStream(
@@ -95,24 +101,29 @@ public class ExternalSort {
         int[] startBlock = new int[numOfBlocks];
         int[] endBlock = new int[numOfBlocks];
 
+        int[] numLeft = new int[numOfBlocks];
         for(int i=0; i<numOfBlocks; i++){
-            startBlock[i]=i*blockSize;
-            endBlock[i]=Math.min((i+1)*blockSize,numOfInts);
+            numLeft[i]=blockSize-1;
+            //startBlock[i]=i*blockSize;
+            //endBlock[i]=Math.min((i+1)*blockSize,numOfInts);
             //System.out.println(startBlock[i] + " " + endBlock[i] + " ");
             dis[i] = new DataInputStream(
                     new BufferedInputStream(new FileInputStream(file1)));
-            dis[i].skipBytes(INT_BYTES*startBlock[i]);
+            dis[i].skipBytes(INT_BYTES*i*blockSize);
+
             //System.out.println(dis[i].readInt());
             pq.add(new PriorityQueueElement(dis[i].readInt(), i));
         }
-        
+        numLeft[numOfBlocks-1]=numOfInts-(numOfBlocks-1)*blockSize-1;
         //System.out.println(pq);
+        int idx;
         while(pq.size()>0){
             PriorityQueueElement top=pq.poll();
             dos.writeInt(top.getVal());
-            if(startBlock[top.getIdx()]+1 < endBlock[top.getIdx()]){
-                startBlock[top.getIdx()]++;
-                pq.add(new PriorityQueueElement(dis[top.getIdx()].readInt(), top.getIdx()));
+            idx=top.getIdx();
+            if(numLeft[idx]>0){
+                numLeft[idx]--;
+                pq.add(new PriorityQueueElement(dis[idx].readInt(), idx));
             }
             //System.out.println(pq);
         }
@@ -123,85 +134,78 @@ public class ExternalSort {
         RandomAccessFile f = null;
         DataInputStream d = null;
         RandomAccessFile raf2 = new RandomAccessFile(f2,"rw");
-        DataOutputStream dos = new DataOutputStream(
-                new BufferedOutputStream(new FileOutputStream(f2,false)));
         f = new RandomAccessFile(f1,"r");
         int min=Integer.MAX_VALUE, max=Integer.MIN_VALUE;
         d = new DataInputStream(
                 new BufferedInputStream(new FileInputStream(f.getFD())));
+        DataOutputStream dos = new DataOutputStream(
+                new BufferedOutputStream(new FileOutputStream(f2,false)));
 
-        ArrayList<Integer> lst = new ArrayList<Integer>();
-        HashMap<Integer, Integer> cntSortMap = new HashMap<Integer, Integer>();
-        int k,cnt=0,prev=Integer.MIN_VALUE;
+        int[] arr=new int[BLOCK_SIZE];
+        int[] cntSort= new int[2*OFFSET+1];
+        int k,prev=Integer.MIN_VALUE;
         int cntElements=0;
+        int numOfInts = (int)(f.length() >> 2);
         boolean sorted=true;
-        f.seek(0);
+        boolean smallEnough=true;
         //System.out.println(f.length());
         //System.out.println(f.readInt()+ " ibaah " +f.readInt());
-        while (d.available() > 0) {
+        for(int i=0; i<numOfInts; i++){
             k=d.readInt();
-            lst.add(k);
+            arr[cntElements]=k;
             cntElements++;
-            if(CNT_MAX==cntElements){
-                Collections.sort(lst);
-                for(int i:lst){
-                    dos.writeInt(i);
+            if(BLOCK_SIZE==cntElements){
+                if(smallEnough==false){
+                    Arrays.sort(arr);
+                    for(int p:arr){
+                        dos.writeInt(p);
+                    }
+                    dos.flush();
                 }
-                dos.flush();
-                lst.clear();
                 cntElements=0;
             }
-            if(RANGE_MIN<=k && RANGE_MAX>=k){
-                if( !cntSortMap.containsKey(k) ){
-                    cntSortMap.put(k, 1);
-                }
-                else {
-                    cntSortMap.put(k, cntSortMap.get(k)+1);
-                }
+            if(smallEnough == true && RANGE_MIN<=k && RANGE_MAX>=k){
+                cntSort[OFFSET+k]++;
             }
+            else 
+                smallEnough=false;
             //System.out.print(k+" ");
-            if (k<prev)
-                sorted=false;
-            prev=k;
-            if (k<min)
-                min=k;
-            if (k>max)
-                max=k;
-            cnt++;
+            if(smallEnough){
+                if (k<min)
+                    min=k;
+                if (k>max)
+                    max=k;
+            }
         }
+        //System.out.println(f.length());
         if(cntElements!=0){
-            if(cnt<=CNT_MAX){
+            if(f.length()<=INT_BYTES*BLOCK_SIZE){
                 //thats the case when all input numbers are less than 1e5
                 dos.close();
                 dos = new DataOutputStream(
                     new BufferedOutputStream(new FileOutputStream(f1,false)));
 
             }
-            Collections.sort(lst);
-            for(int i:lst){
-                dos.writeInt(i);
+            Arrays.sort(arr,0,cntElements);
+            for(int i=0; i<cntElements; i++){
+                //System.out.println(i);
+                dos.writeInt(arr[i]);
             }
             dos.flush();
-            lst.clear();
             cntElements=0;
             //thats the case when all input numbers are less than 1e5
-            if(cnt<=CNT_MAX)
+            if(f.length()<=INT_BYTES*BLOCK_SIZE)
                 return;
         }
-        if (sorted) {
-            //System.err.println("SORTED");
-            return;
-        }
-
         //System.out.println();
         //System.out.println(cnt+"\n"+min+" "+max);
-        if (RANGE_MIN<=min && max<=RANGE_MAX) {
-            countSort(f1,f2,min,max,cntSortMap);
+        if (smallEnough) {
+            countSort(f1,f2,min,max,cntSort);
             return;
         }
         //countSort(f1,f2,min,max);
         //prepare(f1,f2);
-        mergeSort(f2,f1,f1,f2,CNT_MAX);
+        mergeSort(f2,f1,f1,f2,BLOCK_SIZE);
         f.close();
         d.close();
     }
